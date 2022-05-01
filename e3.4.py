ed2a8c278edadd65847f13cdfb8235eadaa2823b
Koushik Dey <koushik.dey@live.com>
2022-05-02 00:30:43 +0300

    e3.4

--- e3.4.py
+++ e3.4.py
@@ -21,3 +21,14 @@
 #Both numbers are odd.
 
 #Solution:
+
+
+number_1 = int(input("Give a number: "))
+number_2 = int(input("Give another number: "))
+
+if number_1 % 2 == 0 and number_2 % 2 == 0:
+    print("Both numbers are even.")
+elif number_1 % 2 == 0 or number_2 % 2 == 0:
+    print("One of the numbers is even.")
+else:
+    print("Both numbers are odd.")
