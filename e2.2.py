d9a19d484071712d3ab2adb53ac88efc14fc56e6
Koushik Dey <koushik.dey@live.com>
2022-05-01 14:39:53 +0300

    e2.2

---
+++ e2.2.py
@@ -0,0 +1,13 @@
+#E2.2
+#Problem:
+#Define two variables and assign them values of 1000 and 24. After this,
+#make the interpreter do the calculation number1+number2+15 and then take the 2nd exponent (**2) of the result.
+#Save this final result to a new variable,
+#and make the program print the result in the following way:
+#The final results of the calculation was: 1079521
+#Solution:
+
+number1 = 1000
+number2 = 24
+number3 = number1 + number2
+print("The final results of the calculation was:",(number3 + 15)**2)
