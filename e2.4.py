ceda3cb4cfec4e4b8170e9196db3561d390c5679
Koushik Dey <koushik.dey@live.com>
2022-05-01 14:48:46 +0300

    e2.4

---
+++ e2.4.py
@@ -0,0 +1,14 @@
+#E2.4
+#Problem:
+#In this program the objective is to understand how the layout-characters (\t and \n} and print in general works.
+#By using only the print command, make the interpreter to print out the following text:
+
+#This here is divided to several lines:
+#I am still in the same print-command.
+         #Name:	Peter
+         #Job:	Babysitter
+#Solution:
+
+
+print("This here is divided to several lines:\n", "I am still in the same print-command.\n", "Name:\t Peter\n"
+      "Job:\t Babysitter")
