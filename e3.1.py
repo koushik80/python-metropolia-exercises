5d3bcd5607ab8e4e32cfc6026e4c0a8ada45cb3f
Koushik Dey <koushik.dey@live.com>
2022-05-01 20:18:57 +0300

    e3.1

---
+++ e3.1.py
@@ -0,0 +1,17 @@
+#E3.1
+#Problem:
+#The program should first ask a number from the user and save it to a variable.
+#Then if the number is even (possible to divide by 2),
+#the program should print the text "The given number was even.".
+#The best way to do this is to use the operator remainder.
+#The program should print the following:
+
+#Give a number: 24
+#The given number was even.
+#or alternatively
+#Give a number: 11
+#Splution:
+
+x = int(input("Give a number: "))
+if (x % 2) == 0:
+    print("The given number was even.")
